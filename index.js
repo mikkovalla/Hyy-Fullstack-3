fcc90f6d65e008d9066c040cfb579ef3810c3aad
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-02-08 23:18:36 +0200

    3.10 heroku deployment done

--- index.js
+++ index.js
@@ -116,6 +116,7 @@ app.post('/api/persons', (req, res) => {
   res.json(hlo)
 })
 
+//port fix for heroku deployment
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
