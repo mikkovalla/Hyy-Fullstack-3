d6b149305ac8cb2b8982644c49861295d28bc389
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-02-08 21:51:12 +0200

    3.6 morgan 'tiny' prints logs

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 app.use(bodyParser.json())
+app.use(morgan('tiny'))
 
 let persons = [{
     "name": "Arto Hellas",
@@ -31,8 +33,14 @@ let persons = [{
     "id": 5
   }
 ]
+
+morgan.token('type', function (req, res) {
+  return req.headers['content-type']
+})
+
 app.get('/', (req, res) => {
   res.send('<h1>puhelinluettelo</h1>')
+  morgan.token()
 })
 
 app.get('/info', (req, res) => {
@@ -49,6 +57,7 @@ app.get('/api/persons/:id', (req, res) => {
 
   if (person) {
     res.json(person)
+    morgan.token()
   } else {
     res.status(404).end()
   }
@@ -71,12 +80,12 @@ app.post('/api/persons', (req, res) => {
   console.log(person)
   console.log(uusiId())
 
-  if(person.name === undefined) {
+  if (person.name === undefined) {
     return res.status(400).json({
       error: 'Henkilöllä pitää olla nimi'
     })
   }
-  if(person.number === undefined) {
+  if (person.number === undefined) {
     return res.status(400).json({
       error: 'puhelin numero puuttuu'
     })
@@ -86,12 +95,12 @@ app.post('/api/persons', (req, res) => {
   const onkoOlemassa = persons.find(p => p.name.toUpperCase() === uusiNimi)
   console.log(onkoOlemassa)
 
-  if(onkoOlemassa) {
+  if (onkoOlemassa) {
     return res.status(400).json({
       error: 'Henkilöllä on jo numero'
     })
   }
-  
+
   const hlo = {
     name: person.name,
     number: person.number,
@@ -101,6 +110,7 @@ app.post('/api/persons', (req, res) => {
   persons = persons.concat(hlo)
   console.log(hlo)
   res.json(hlo)
+  morgan.token()
 })
 
 const PORT = 3001
