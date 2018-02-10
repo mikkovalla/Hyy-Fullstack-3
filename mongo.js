504a6db10ffd52890cb52cbabf04e25748986ec0
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-02-11 00:18:40 +0200

    3.12 done

---
+++ mongo.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const url = process.env.MONGODB_URL
+
+mongoose.connect(url)
+
+const Person = mongoose.model('Person', {
+  name: String,
+  number: String
+})
+
+if (process.argv[2] && process.argv[3]) {
+
+  const person = new Person({
+    name: process.argv[2],
+    number: process.argv[3]
+  })
+
+  person
+    .save()
+    .then(response => {
+      console.log(`lisätään henkilö ${person.name} numero ${person.number} luetteloon`)
+      mongoose.connection.close()
+    })
+} else {
+  console.log('puhelinluettelo: ')
+  Person
+    .find({})
+    .then(pe => {
+      pe.forEach(p => {
+        console.log(`${p.name} ${p.number}`)
+      })
+      mongoose.connection.close()
+    })
+}
